af6b522aa3e4285fef3628409ac0373da9cc6716
MdMerajulHasan <merajuljim1971@gmail.com>
2026-07-09 16:30:56 +0600

    app.json and _layout.tsx and eas.json changed sothat the app don't crush initialy!

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,56 @@
+import * as SplashScreen from "expo-splash-screen";
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { useCallback, useEffect, useState } from "react";
 
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(async () => {
+      try {
+        await SplashScreen.hideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      try {
+        await SplashScreen.hideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <BottomSheetModalProvider>
         <Provider store={store}>
           <StatusBar hidden />
@@ -73,5 +114,3 @@ export default function RootLayout() {
     </GestureHandlerRootView>
   );
 }
-
-
